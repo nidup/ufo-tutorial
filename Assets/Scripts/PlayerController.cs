6304b63c58fe8a13cc7f67faf589ed93296d5aac
Nicolas Dupont <nicolas@akeneo.com>
2016-11-04 15:56:36 +0100

    Chapter 4, count pick up objects and display score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody2D rb2d;
+    private int count;
 
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
+        count = 0;
+        setCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -24,6 +31,15 @@ public class PlayerController : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("PickUp")) {
             other.gameObject.SetActive(false);
+            count = count + 1;
+            setCountText();
+        }
+    }
+
+    void setCountText() {
+        countText.text = "Count : " + count.ToString();
+        if (count >= 8) {
+            winText.text = "You Win!";
         }
     }
 }
